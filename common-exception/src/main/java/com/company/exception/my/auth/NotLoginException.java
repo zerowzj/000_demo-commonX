00ff1555395a224e102be3f74c300266b08949e9
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-10-25 16:57:06 +0800

    更新代码

---
+++ common-exception/src/main/java/com/company/exception/my/auth/NotLoginException.java
@@ -0,0 +1,9 @@
+package com.company.exception.my.auth;
+
+/**
+ * 未登录异常
+ *
+ * @author wangzhj
+ */
+public class NotLoginException extends AuthException {
+}
---
+++ common-exception/src/main/java/com/company/exception/my/auth/UnauthorizedException.java
@@ -0,0 +1,9 @@
+package com.company.exception.my.auth;
+
+/**
+ * 未授权异常
+ *
+ * @author wangzhj
+ */
+public class UnauthorizedException extends AuthException {
+}
