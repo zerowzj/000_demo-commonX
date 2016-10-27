4015ea88f77b9da0694dabe0fe9a614c99f62087
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-10-27 16:14:37 +0800

    更新代码

--- common-db/src/main/java/com/company/db/interceptor/MyInterceptor.java
+++ common-db/src/main/java/com/company/db/interceptor/MyInterceptor.java
@@ -1,6 +1,8 @@
 package com.company.db.interceptor;
 
+import org.apache.ibatis.cache.CacheKey;
 import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
@@ -18,7 +20,8 @@ import java.lang.reflect.Method;
  * @author wangzhj
  */
 @Intercepts({@Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}),
-             @Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class})
+        @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class, CacheKey.class, BoundSql.class}),
+        @Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class})
 })
 public class MyInterceptor extends InterceptorAdapter {
 
