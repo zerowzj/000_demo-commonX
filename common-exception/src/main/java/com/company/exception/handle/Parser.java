f7e979fb263ef35b0e6815217db8640da09a1947
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-10-28 16:35:38 +0800

    更新代码

--- common-exception/src/main/java/com/company/exception/handle/Parser.java
+++ common-exception/src/main/java/com/company/exception/handle/Parser.java
@@ -1,9 +1,48 @@
 package com.company.exception.handle;
 
+import com.company.exception.domain.AuthException;
+import com.company.exception.domain.BaseException;
+import com.company.exception.domain.BusinessException;
+import com.company.exception.domain.ParamException;
+import com.google.common.base.Joiner;
+
 /**
- * Created by wangzhj on 2016/10/28.
+ *
+ *
+ * @author  wangzhj
  */
 public class Parser {
 
 
+    public static String getLogMsg() {
+
+    }
+
+    public String handle(BaseException ex) {
+        //获取错误码和错误描述
+        String errorCode = ex.getErrorCode();
+        String errorDesc = ex.getErrorDesc();
+        //
+        if (ex instanceof AuthException) {
+
+
+        } else if (ex instanceof ParamException) {
+            ParamException pex = (ParamException) ex;
+            pex.getParamName();
+            pex.getParamValue();
+
+        } else if (ex instanceof BusinessException) {
+            BusinessException bex = (BusinessException) ex;
+        }
+
+
+    }
+
+    private String getFormat(int count) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i <= count; i++) {
+            sb.append("{").append(i).append("}, ");
+        }
+        return "[" + sb.substring(0, sb.length() - 2) + "]";
+    }
 }
