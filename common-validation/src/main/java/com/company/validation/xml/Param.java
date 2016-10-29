9db1cfc278bcd72d6243829482461835580f236b
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-10-29 22:10:24 +0800

    更新代码

---
+++ common-validation/src/main/java/com/company/validation/xml/Param.java
@@ -0,0 +1,13 @@
+package com.company.validation.xml;
+
+/**
+ * 参数
+ *
+ * @author wangzhj
+ */
+public class Param {
+
+    /** 参数名 */
+    private String paramName = null;
+
+}
---
+++ common-validation/src/main/java/com/company/validation/xml/ParamSet.java
@@ -0,0 +1,25 @@
+package com.company.validation.xml;
+
+import java.util.List;
+
+/**
+ * 参数集合
+ *
+ * @author wangzhj
+ */
+public class ParamSet {
+
+	/** 关键字 */
+	private String key = null;
+
+	/** 参数列表 */
+	private List<Param> paramLt = null;
+
+	public List<Param> getParamLt() {
+		return paramLt;
+	}
+
+	public void setParamLt(List<Param> paramLt) {
+		this.paramLt = paramLt;
+	}
+}
