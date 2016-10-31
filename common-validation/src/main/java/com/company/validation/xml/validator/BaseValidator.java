f00d3a9959b0e271bc079b23233884f20f90f425
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-10-31 10:26:06 +0800

    更新代码

--- common-validation/src/main/java/com/company/validation/xml/validator/BaseValidator.java
+++ common-validation/src/main/java/com/company/validation/xml/validator/BaseValidator.java
@@ -1,5 +1,6 @@
 package com.company.validation.xml.validator;
 
+import com.company.exception.domain.param.EmptyValueException;
 import com.company.validation.xml.Param;
 import com.company.validation.xml.rule.BaseRule;
 import com.company.validation.xml.rule.Rule;
@@ -7,7 +8,9 @@ import com.company.validation.xml.rule.Rule;
 import java.util.List;
 
 /**
- * Created by wangzhj on 2016/10/30.
+ * 基础验证器
+ *
+ * @author wangzhj
  */
 public abstract class BaseValidator implements Validator {
 
@@ -15,12 +18,18 @@ public abstract class BaseValidator implements Validator {
     public final void validate(Param param, Object value) {
         List<Rule> ruleLt = param.getRuleLt();
         BaseRule baseRule = null;
-        if(baseRule.isNotEmpty() && value == null) {
-
+        if (baseRule.isNotEmpty() && value == null) {
+            throw new EmptyValueException(param.getParamName());
         }
-
         validateCustom(param, value);
     }
 
+    /**
+     * 自定义验证
+     *
+     * @param param
+     * @param value
+     * @return boolean
+     */
     public abstract void validateCustom(Param param, Object value);
 }
--- common-validation/src/main/java/com/company/validation/xml/validator/Validator.java
+++ common-validation/src/main/java/com/company/validation/xml/validator/Validator.java
@@ -3,12 +3,27 @@ package com.company.validation.xml.validator;
 import com.company.validation.xml.Param;
 
 /**
- * Created by wangzhj on 2016/10/30.
+ * 验证器
+ *
+ * @author wangzhj
  */
 public interface Validator {
 
+    /**
+     * 支持
+     *
+     * @param obj
+     * @return boolean
+     */
     public boolean support(Object obj);
 
+    /**
+     * 验证
+     *
+     * @param param
+     * @param value
+     * @return boolean
+     */
     public void validate(Param param, Object value);
 
 }
