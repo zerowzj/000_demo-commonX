c15975eb77b8b53ec2acfdfa13a78e40bb4cc30c
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-10-31 17:55:08 +0800

    更新代码

--- common-exception/src/main/java/com/company/exception/ExceptionUtil.java
+++ common-exception/src/main/java/com/company/exception/ExceptionUtil.java
@@ -3,5 +3,24 @@ package com.company.exception;
 /**
  * Created by wangzhj on 2016/10/31.
  */
-public class ExceptionUtil {
+public abstract class ExceptionUtil {
+
+    public static String getErrorDesc(BaseException ex) {
+
+        return null;
+    }
+
+    public static String getFmtErrorDesc(BaseException ex) {
+        String errorDesc = ex.getErrorDesc();
+        String fmtErrorDesc = ex.getFmtErrorDesc();
+        if (ex instanceof ParamException) {
+            ParamException pex = (ParamException) ex;
+            pex.getParamName();
+            pex.getParamValue();
+
+        } else if(ex instanceof BusinessException){
+
+        }
+        return null;
+    }
 }
