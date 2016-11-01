c3703bef819e8464325e4bb5b949b4f2b3404fb8
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-11-01 13:18:30 +0800

    更新代码

---
+++ common-validation/src/test/java/com/company/exception/EnumRuleTest.java
@@ -0,0 +1,7 @@
+package com.company.exception;
+
+/**
+ * Created by wangzhj on 2016/11/1.
+ */
+public class EnumRuleTest {
+}
---
+++ common-validation/src/test/java/com/company/exception/StringRuleTest.java
@@ -0,0 +1,14 @@
+package com.company.exception;
+
+import org.junit.Test;
+
+/**
+ * Created by wangzhj on 2016/11/1.
+ */
+public class StringRuleTest {
+
+    @Test
+    public void test(){
+
+    }
+}
