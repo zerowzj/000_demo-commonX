953f6693c1309a1dee56ae210e227648936922f8
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-11-02 18:38:41 +0800

    更新代码

---
+++ common-util/src/main/java/com/company/util/DateUtil.java
@@ -0,0 +1,33 @@
+package com.company.util;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * Date Util
+ *
+ * @author wangzhj
+ */
+public abstract class DateUtil {
+
+    /**
+     * 是否是指定格式日期
+     *
+     * @param dateStr
+     * @param format
+     * @return boolean
+     */
+    public static boolean isFormatDate(String dateStr, String format) {
+        if (dateStr == null || format == null) {
+            return false;
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        boolean be = true;
+        try {
+            sdf.setLenient(false);
+            sdf.parse(dateStr);
+        } catch (Exception ex) {
+            be = false;
+        }
+        return be;
+    }
+}
