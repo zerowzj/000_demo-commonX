ff2791b8d5bfe596c8046dbc5672eee1fbea6635
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-11-02 09:53:30 +0800

    更新代码

--- common-validation/src/main/java/com/company/validation/xml/validator/DateValidator.java
+++ common-validation/src/main/java/com/company/validation/xml/validator/DateValidator.java
@@ -9,6 +9,7 @@ import org.joda.time.Interval;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -41,17 +42,38 @@ public class DateValidator implements Validator {
      /*   if(dtFormat){
             throw new FormatErrorException(paramName, value);
         }*/
-        //
+        //最小日期
         DateTime date = new DateTime(value);
         String minDate = rule.getMinDate();
         if (minDate != null && date.isBefore(new DateTime(minDate))) {
             detail.append("");
             throw new ValueIllegalException(detail.toString(), paramName, value);
         }
+        //最大日期
         String maxDate = rule.getMaxDate();
         if (maxDate != null && date.isAfter(new DateTime(minDate))) {
             detail.append("");
             throw new ValueIllegalException(detail.toString(), paramName, value);
         }
+    }
+
+    private boolean isFormat(String format, String dateStr) {
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        boolean f = true;
+        try {
+            sdf.parse(dateStr);
+        } catch (Exception ex) {
+            f = false;
+        }
+        return f;
+    }
+
+    public static void main(String[] args) {
+        String dateString = "2009-04-17";
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("yyy");
+        System.out.println(formatter.isParser());
+        System.out.println(formatter.isPrinter());
+
+
     }
 }
