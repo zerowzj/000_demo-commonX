d39e2a03293021e4dec05d2be9aeff54abe8974e
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-11-10 11:50:33 +0800

    更新代码

---
+++ common-exception/src/main/java/com/company/exception/auth/CaptchaErrorException.java
@@ -0,0 +1,22 @@
+package com.company.exception.auth;
+
+import com.company.exception.AuthException;
+
+import static com.company.exception.MessageUtil.format;
+
+/**
+ * <p>验证码错误异常<／p>
+ *
+ * @author wangzhj
+ * @time 2016-11-10 11:48
+ */
+public class CaptchaErrorException extends AuthException {
+
+    private static final String ERROR_CODE = "1002";
+
+    private static final String DESC_KEY = "auth.error.captchaError";
+
+    public CaptchaErrorException() {
+        super(ERROR_CODE, format(DESC_KEY));
+    }
+}
