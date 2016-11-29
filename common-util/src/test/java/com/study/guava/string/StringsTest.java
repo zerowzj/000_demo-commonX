a2d1125b1e9fb9d325c8e274df5ed4f292a810bc
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-11-29 17:00:21 +0800

    更新代码

---
+++ common-util/src/test/java/com/study/guava/string/StringsTest.java
@@ -0,0 +1,23 @@
+package com.study.guava.string;
+
+import com.google.common.base.Strings;
+import org.junit.Test;
+
+/**
+ * <p></p>
+ *
+ * @author wangzhj
+ * @time 2016-11-29 16:56
+ */
+public class StringsTest {
+
+    @Test
+    public void test(){
+        System.out.println(Strings.emptyToNull(null));
+        System.out.println(Strings.emptyToNull(" "));
+
+        System.out.println(Strings.isNullOrEmpty(null));
+        System.out.println(Strings.isNullOrEmpty("  "));
+
+    }
+}
