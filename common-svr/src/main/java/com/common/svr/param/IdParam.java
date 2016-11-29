4bb6d09b72c9b20752163e5c3fc576f142e0d231
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-11-29 11:15:15 +0800

    更新代码

---
+++ common-svr/src/main/java/com/common/svr/param/IdParam.java
@@ -0,0 +1,21 @@
+package com.common.svr.param;
+
+/**
+ * <p></p>
+ *
+ * @author wangzhj
+ * @time 2016-11-29 11:12
+ */
+public class IdParam extends BaseParam {
+
+    /*  **/
+    private Long id = null;
+
+    public IdParam(Long id){
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
