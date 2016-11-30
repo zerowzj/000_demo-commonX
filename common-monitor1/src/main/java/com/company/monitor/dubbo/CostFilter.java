4a6e2ba0cce589c92affac11f8e40b6bec880319
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-11-30 19:47:23 +0800

    更新代码

--- common-monitor1/src/main/java/com/company/monitor/dubbo/CostFilter.java
+++ common-monitor1/src/main/java/com/company/monitor/dubbo/CostFilter.java
@@ -1,5 +1,6 @@
 package com.company.monitor.dubbo;
 
+import com.alibaba.dubbo.common.extension.Activate;
 import com.alibaba.dubbo.rpc.Filter;
 import com.alibaba.dubbo.rpc.Invocation;
 import com.alibaba.dubbo.rpc.Invoker;
@@ -12,6 +13,7 @@ import com.alibaba.dubbo.rpc.RpcException;
  * @author wangzhj
  * @time 2016-11-30 18:55
  */
+@Activate(group = {com.alibaba.dubbo.common.Constants.PROVIDER, com.alibaba.dubbo.common.Constants.CONSUMER})
 public class CostFilter implements Filter {
 
     @Override
