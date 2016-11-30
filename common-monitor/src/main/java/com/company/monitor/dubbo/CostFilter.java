fe6dfffc0d76de0b5a122a5f4bda94f871097ab3
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-11-30 19:01:22 +0800

    更新代码

---
+++ common-monitor/src/main/java/com/company/monitor/dubbo/CostFilter.java
@@ -0,0 +1,12 @@
+package com.company.monitor.dubbo;
+
+//import com.alibaba.dubbo.rpc.Filter;
+
+/**
+ * <p></p>
+ *
+ * @author wangzhj
+ * @time 2016-11-30 18:55
+ */
+public class CostFilter {
+}
