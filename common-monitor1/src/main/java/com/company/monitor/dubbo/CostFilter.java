7f5e9f29cce7e2b75799e8572060a523d5294b03
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-11-30 19:51:10 +0800

    更新代码

--- common-monitor1/src/main/java/com/company/monitor/dubbo/CostFilter.java
+++ common-monitor1/src/main/java/com/company/monitor/dubbo/CostFilter.java
@@ -1,10 +1,12 @@
 package com.company.monitor.dubbo;
 
+import com.alibaba.dubbo.common.Constants;
 import com.alibaba.dubbo.common.extension.Activate;
 import com.alibaba.dubbo.rpc.Filter;
 import com.alibaba.dubbo.rpc.Invocation;
 import com.alibaba.dubbo.rpc.Invoker;
 import com.alibaba.dubbo.rpc.Result;
+import com.alibaba.dubbo.rpc.RpcContext;
 import com.alibaba.dubbo.rpc.RpcException;
 
 /**
@@ -13,11 +15,15 @@ import com.alibaba.dubbo.rpc.RpcException;
  * @author wangzhj
  * @time 2016-11-30 18:55
  */
-@Activate(group = {com.alibaba.dubbo.common.Constants.PROVIDER, com.alibaba.dubbo.common.Constants.CONSUMER})
+@Activate(group = {Constants.PROVIDER, Constants.CONSUMER})
 public class CostFilter implements Filter {
 
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+        RpcContext context = RpcContext.getContext();
+        if(context.isConsumerSide()){
+
+        }
         return null;
     }
 }
