22846484b2b4420a42733b33b63d4c96d438d460
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-12-01 10:44:57 +0800

    更新代码

--- common-monitor/src/main/java/com/company/monitor/dubbo/ConsumerFilter.java
+++ common-monitor/src/main/java/com/company/monitor/dubbo/ConsumerFilter.java
@@ -8,6 +8,8 @@ import com.alibaba.dubbo.rpc.Invoker;
 import com.alibaba.dubbo.rpc.Result;
 import com.alibaba.dubbo.rpc.RpcContext;
 import com.alibaba.dubbo.rpc.RpcException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * <p></p>
@@ -18,9 +20,13 @@ import com.alibaba.dubbo.rpc.RpcException;
 @Activate(group = {Constants.CONSUMER})
 public class ConsumerFilter implements Filter {
 
+    private static final Logger logger = LoggerFactory.getLogger(ConsumerFilter.class);
+
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         RpcContext context = RpcContext.getContext();
+        invocation.getMethodName();
+        invocation.getArguments();
         return null;
     }
 }
