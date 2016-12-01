86e5632e8298b0c76caec9eb1026dc382b2580a0
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2016-12-01 18:41:41 +0800

    更新代码

---
+++ common-monitor/src/main/java/com/company/monitor/dubbo/CostTimeFilter.java
@@ -0,0 +1,48 @@
+package com.company.monitor.dubbo;
+
+import com.alibaba.dubbo.common.Constants;
+import com.alibaba.dubbo.common.extension.Activate;
+import com.alibaba.dubbo.rpc.Filter;
+import com.alibaba.dubbo.rpc.Invocation;
+import com.alibaba.dubbo.rpc.Invoker;
+import com.alibaba.dubbo.rpc.Result;
+import com.alibaba.dubbo.rpc.RpcContext;
+import com.alibaba.dubbo.rpc.RpcException;
+import com.company.util.JsonUtil;
+import com.google.common.base.Joiner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <p>耗时过滤器</p>
+ *
+ * @author wangzhj
+ * @time 2016-11-30 18:55
+ */
+@Activate(group = {Constants.CONSUMER, Constants.PROVIDER})
+public class CostTimeFilter implements Filter {
+
+    private static final Logger logger = LoggerFactory.getLogger(CostTimeFilter.class);
+
+    @Override
+    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+        long start = System.currentTimeMillis();
+        //接口全限定名
+        String canonicalName = invoker.getInterface().getCanonicalName();
+        String methodName = invocation.getMethodName();
+        String fqName = Joiner.on(".").join(canonicalName, methodName);
+        //
+        RpcContext context = RpcContext.getContext();
+        logger.info(JsonUtil.toJson(invocation.getArguments()));
+        Result result = null;
+        try {
+            result = invoker.invoke(invocation);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            long end = System.currentTimeMillis();
+            logger.info("Dubbo Interface[{}] [COST TIME] [{}] s", fqName, (end - start) / 1000.00D);
+        }
+        return result;
+    }
+}
