fccf43fbd6c2c7fdf16c58ba7b55339149f8b606
wangzhenjun <wangzhenjun@okay.cn>
2016-12-14 15:22:27 +0800

    更新代码

--- common-util/src/test/java/com/study/jedis/lua/user_login.lua
+++ common-util/src/test/java/com/study/jedis/lua/user_login.lua
@@ -6,22 +6,22 @@
 -- To change this template use File | Settings | File Templates.
 --
 
-local key = "user:login:" .. KEYS[1];
+local key = KEYS[1];
 
 local is_exists = redis.call("EXISTS", key)
 if is_exists == 0 then
     redis.call("SET", key, 1)
     redis.call("EXPIRE", key, 60)
-    redis.log(redis.LOG_NOTICE, "不存在")
+    redis.log(redis.LOG_NOTICE, "key["..key.."] not exists, set it")
     return 0
 else
     local counter = redis.call("GET", key)
     if tonumber(counter) >= 3 then
-        redis.log(redis.LOG_NOTICE, "3次以上")
+        redis.log(redis.LOG_NOTICE, "key["..key.."] times over")
         return 1
     else
         redis.call("INCR", key)
-        redis.log(redis.LOG_NOTICE, "+1")
+        redis.log(redis.LOG_NOTICE, "key["..key.."] incr 1")
         return 0
     end
 end
