1af69d6e8d2249656494d57abb5a64f6034709b9
wangzhenjun <wangzhenjun@okay.cn>
2016-12-14 13:18:21 +0800

    更新代码

--- common-util/src/test/java/com/study/jedis/lua/LuaTest.java
+++ common-util/src/test/java/com/study/jedis/lua/LuaTest.java
@@ -7,7 +7,8 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.File;
+import java.io.Reader;
 import java.nio.charset.Charset;
 import java.util.Arrays;
 
