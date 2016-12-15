c19cc37e4c5f03d64e1dfe51a0d5976dbd520415
wangzhenjun <wangzhenjun@okay.cn>
2016-12-15 15:34:32 +0800

    更新代码

--- common-util/src/test/java/com/study/jedis/data/JedisMapTest.java
+++ common-util/src/test/java/com/study/jedis/data/JedisMapTest.java
@@ -17,7 +17,18 @@ public class JedisMapTest extends JedisBaseTest {
 
     private static final Logger logger = LoggerFactory.getLogger(JedisMapTest.class);
 
-    //==================== 放值 ====================
+    //==============================
+    //             放值
+    //==============================
+
+    /**
+     * 将哈希表key中的域field的值设为value。
+     * 如果key不存在，一个新的哈希表被创建并进行HSET操作。
+     * 如果域field已经存在于哈希表中，旧值将被覆盖。
+     *
+     * @return Long 如果field是哈希表中的一个新建域，并且值设置成功，返回1。
+     * 如果哈希表中域field已经存在且旧值已被新值覆盖，返回0。
+     */
     @Test
     public void test_hset() {
         jedis = jedisPool.getResource();
@@ -26,6 +37,28 @@ public class JedisMapTest extends JedisBaseTest {
         logger.info(number + "");
     }
 
+    /**
+     * 将哈希表key中的域field的值设置为value，当且仅当域field不存在。
+     * 若域field已经存在，该操作无效。
+     * 如果key不存在，一个新哈希表被创建并执行HSETNX命令。
+     *
+     * @return Long 设置成功，返回1。如果给定域已经存在且没有操作被执行，返回0。
+     */
+    @Test
+    public void test_hsetnx() {
+        jedis = jedisPool.getResource();
+        jedis.hset("hset", "name", "wangzhenjun");
+        Long number = jedis.hsetnx("hset", "age", "33");
+        logger.info(number + "");
+    }
+
+    /**
+     * 同时将多个field - value(域-值)对设置到哈希表key中。
+     * 此命令会覆盖哈希表中已存在的域。
+     * 如果key不存在，一个空哈希表被创建并执行HMSET操作。
+     *
+     * @return String 如果命令执行成功，返回OK。当key不是哈希表(hash)类型时，返回一个错误。
+     */
     @Test
     public void test_hmset() {
         jedis = jedisPool.getResource();
@@ -36,7 +69,15 @@ public class JedisMapTest extends JedisBaseTest {
         logger.info(str);
     }
 
-    //==================== 取值 ====================
+    //==============================
+    //             取值
+    //==============================
+
+    /**
+     * 返回哈希表key中给定域field的值。
+     *
+     * @return String 给定域的值。当给定域不存在或是给定key不存在时，返回nil。
+     */
     @Test
     public void test_hget() {
         jedis = jedisPool.getResource();
@@ -44,6 +85,13 @@ public class JedisMapTest extends JedisBaseTest {
         logger.info(str);
     }
 
+    /**
+     * 返回哈希表key中，一个或多个给定域的值。
+     * 如果给定的域不存在于哈希表，那么返回一个nil值。
+     * 因为不存在的key被当作一个空哈希表来处理，所以对一个不存在的key进行HMGET操作将返回一个只带有nil值的表。
+     *
+     * @return List 一个包含多个给定域的关联值的表，表值的排列顺序和给定域参数的请求顺序一样。
+     */
     @Test
     public void test_hmget() {
         jedis = jedisPool.getResource();
@@ -51,6 +99,12 @@ public class JedisMapTest extends JedisBaseTest {
         logger.info(valutLt + "");
     }
 
+    /**
+     * 返回哈希表key中，所有的域和值。
+     * 在返回值里，紧跟每个域名(field name)之后是域的值(value)，所以返回值的长度是哈希表大小的两倍。
+     *
+     * @return Lit 以列表形式返回哈希表的域和域的值。 若key不存在，返回空列表。
+     */
     @Test
     public void test_hgetAll() {
         jedis = jedisPool.getResource();
@@ -58,25 +112,54 @@ public class JedisMapTest extends JedisBaseTest {
         logger.info(valutLt + "");
     }
 
-    //==================== key和value ====================
+    /**
+     * 返回哈希表key中的所有域。
+     *
+     * @return Set 一个包含哈希表中所有域的表。当key不存在时，返回一个空表。
+     */
     @Test
-    public void test_hget1() {
+    public void test_hkeys() {
         jedis = jedisPool.getResource();
-        //key的个数
-        Long length = jedis.hlen("user");
-        logger.info(length + "");
-        //key
-        Set<String> keys = jedis.hkeys("user");
-        logger.info(keys.toString());
-        //value
-        List<String> values = jedis.hvals("user");
-        logger.info(values.toString());
+        Set<String> valutLt = jedis.hkeys("user");
+        logger.info(valutLt + "");
     }
 
     /**
-     * ==============================
-     * 存在
-     * ==============================
+     * 返回哈希表key中的所有值。
+     *
+     * @return List 一个包含哈希表中所有值的表。当key不存在时，返回一个空表。
+     */
+    @Test
+    public void test_hvals() {
+        jedis = jedisPool.getResource();
+        List<String> valutLt = jedis.hvals("user");
+        logger.info(valutLt + "");
+    }
+
+    //==============================
+    //             删除
+    //==============================
+
+    /**
+     * 删除哈希表key中的一个或多个指定域，不存在的域将被忽略。
+     *
+     * @return 被成功移除的域的数量，不包括被忽略的域。
+     */
+    @Test
+    public void test_hdel() {
+        jedis = jedisPool.getResource();
+        Long statusCode = jedis.hdel("user", "sss");
+        logger.info(statusCode + "");
+    }
+
+    //==============================
+    //             存在
+    //==============================
+
+    /**
+     * 查看哈希表key中，给定域field是否存在。
+     *
+     * @return 如果哈希表含有给定域，返回1。如果哈希表不含有给定域，或key不存在，返回0。
      */
     @Test
     public void test_hexists() {
@@ -85,15 +168,20 @@ public class JedisMapTest extends JedisBaseTest {
         logger.info(statusCode + "");
     }
 
+    //==============================
+    //             域数量
+    //==============================
+
     /**
-     * ==============================
-     * 删除
-     * ==============================
+     * 返回哈希表key中域的数量。
+     *
+     * @return 哈希表中域的数量。当key不存在时，返回0。
      */
     @Test
-    public void test_hdel() {
+    public void test_hlen() {
         jedis = jedisPool.getResource();
-        Long statusCode = jedis.hdel("user", "sss");
-        logger.info(statusCode + "");
+        Long count = jedis.hlen("user");
+        logger.info(count + "");
     }
+
 }
