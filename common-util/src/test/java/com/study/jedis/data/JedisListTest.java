5cb4ea4da8644c0d9c7015ea3a4af673863a0107
wangzhenjun <wangzhenjun@okay.cn>
2016-12-15 15:09:28 +0800

    更新代码

--- common-util/src/test/java/com/study/jedis/data/JedisListTest.java
+++ common-util/src/test/java/com/study/jedis/data/JedisListTest.java
@@ -15,18 +15,18 @@ public class JedisListTest extends JedisBaseTest {
 
     private static final Logger logger = LoggerFactory.getLogger(JedisListTest.class);
 
-    /**
-     * ==============================
-     * 放值
-     * ==============================
-     */
+    //==============================
+    //             放值
+    //==============================
+
     /**
      * 将一个或多个值value插入到列表key的表头。
      * 如果有多个value值，那么各个value值按从左到右的顺序依次插入到表头：
      * 比如对一个空列表(mylist)执行LPUSH mylist a b c，则结果列表为c b a，等同于执行执行命令LPUSH mylist a、LPUSH mylist b、LPUSH mylist c。
      * 如果key不存在，一个空列表会被创建并执行LPUSH操作。
      * 当key存在但不是列表类型时，返回一个错误。
-     * 执行LPUSH命令后，列表的长度。
+     *
+     * @return Long 执行LPUSH命令后，列表的长度。
      */
     @Test
     public void test_lpush() {
@@ -42,7 +42,8 @@ public class JedisListTest extends JedisBaseTest {
     /**
      * 将值value插入到列表key的表头，当且仅当key存在并且是一个列表。
      * 和LPUSH命令相反，当key不存在时，LPUSHX命令什么也不做。
-     * LPUSHX命令执行之后，表的长度。
+     *
+     * @return Long LPUSHX命令执行之后，表的长度。
      */
     @Test
     public void test_lpushx() {
@@ -57,7 +58,8 @@ public class JedisListTest extends JedisBaseTest {
      * 比如对一个空列表(mylist)执行RPUSH mylist a b c，则结果列表为a b c，等同于执行命令RPUSH mylist a、RPUSH mylist b、RPUSH mylist c。
      * 如果key不存在，一个空列表会被创建并执行RPUSH操作。
      * 当key存在但不是列表类型时，返回一个错误。
-     * 执行RPUSH操作后，表的长度。
+     *
+     * @return Long 执行RPUSH操作后，表的长度。
      */
     @Test
     public void test_rpush() {
@@ -69,7 +71,8 @@ public class JedisListTest extends JedisBaseTest {
     /**
      * 将值value插入到列表key的表尾，当且仅当key存在并且是一个列表。
      * 和RPUSH命令相反，当key不存在时，RPUSHX命令什么也不做。
-     * RPUSHX命令执行之后，表的长度。
+     *
+     * @return Long RPUSHX命令执行之后，表的长度。
      */
     @Test
     public void test_rpushx() {
@@ -78,17 +81,15 @@ public class JedisListTest extends JedisBaseTest {
         logger.info(number + "");
     }
 
-    @Test
-    public void test_linsert() {
-        jedis = jedisPool.getResource();
-        Long str = jedis.linsert("key1", BinaryClient.LIST_POSITION.BEFORE, "100", "");
-        logger.info(str + "");
-    }
+
+    //==============================
+    //           取值
+    //==============================
 
     /**
-     * ==============================
-     * 取值
-     * ==============================
+     * 移除并返回列表key的头元素。
+     *
+     * @return 列表的头元素。当key不存在时，返回nil。
      */
     @Test
     public void test_lpop() {
@@ -98,6 +99,18 @@ public class JedisListTest extends JedisBaseTest {
     }
 
     @Test
+    public void test_blpop() {
+        jedis = jedisPool.getResource();
+        List<String> value = jedis.blpop(100, "");
+        logger.info(value + "");
+    }
+
+    /**
+     * 移除并返回列表key的尾元素。
+     *
+     * @return 列表的尾元素。当key不存在时，返回nil。
+     */
+    @Test
     public void test_rpop() {
         jedis = jedisPool.getResource();
         String value = jedis.rpop("rpush");
@@ -105,35 +118,58 @@ public class JedisListTest extends JedisBaseTest {
     }
 
     @Test
+    public void test_brpop() {
+        jedis = jedisPool.getResource();
+        List<String> value = jedis.brpop(100, "");
+        logger.info(value + "");
+    }
+
+    /**
+     * 返回列表key中指定区间内的元素，区间以偏移量start和stop指定。
+     * 下标(index)参数start和stop都以0为底，也就是说，以0表示列表的第一个元素，以1表示列表的第二个元素，以此类推。
+     * 你也可以使用负数下标，以-1表示列表的最后一个元素，-2表示列表的倒数第二个元素，以此类推。
+     *
+     * @return List 一个列表，包含指定区间内的元素。
+     */
+    @Test
     public void test_lrang() {
         jedis = jedisPool.getResource();
         List<String> valueLt = jedis.lrange("lpush", 0, 2);
         logger.info(valueLt + "");
     }
 
+    /**
+     * 返回列表key中，下标为index的元素。
+     * 下标(index)参数start和stop都以0为底，也就是说，以0表示列表的第一个元素，以1表示列表的第二个元素，以此类推。
+     * 你也可以使用负数下标，以-1表示列表的最后一个元素，-2表示列表的倒数第二个元素，以此类推。
+     * 如果key不是列表类型，返回一个错误
+     *
+     * @return 列表中下标为index的元素。如果index参数的值不在列表的区间范围内(out of range)，返回nil。
+     */
     @Test
     public void test_lindex() {
         jedis = jedisPool.getResource();
-        String value = jedis.lindex("lpush", 0);
+        String value = jedis.lindex("lpush", 4);
         logger.info(value + "");
     }
+
+
+    // ==============================
+    //              改值
+    // ==============================
 
     @Test
-    public void test_ltrim() {
+    public void test_linsert() {
         jedis = jedisPool.getResource();
-        String value = jedis.ltrim("lpush", 0, 2);
-        logger.info(value + "");
-    }
+        Long str = jedis.linsert("key1", BinaryClient.LIST_POSITION.BEFORE, "100", "");
 
-    /**
-     * ==============================
-     * <p/>
-     * ==============================
-     */
+        logger.info(str + "");
+    }
+    
     @Test
     public void test_lset() {
         jedis = jedisPool.getResource();
-        String str = jedis.lset("key1", 0, "");
+        String str = jedis.lset("lpush", 0, "tttttttttttttt");
         logger.info(str);
     }
 
@@ -143,24 +179,32 @@ public class JedisListTest extends JedisBaseTest {
         logger.info(jedis.ltrim("key1", 0, 3));
     }
 
+    /**
+     * 返回列表key的长度。
+     * 如果key不存在，则key被解释为一个空列表，返回0.
+     * 如果key不是列表类型，返回一个错误。
+     *
+     * @return Long 列表key的长度。
+     */
     @Test
-    public void test() {
+    public void test_llen() {
         jedis = jedisPool.getResource();
         //长度
-        Long length = jedis.llen("");
+        Long length = jedis.llen("lpush");
         logger.info("" + length);
     }
 
+    // ==============================
+    //              改值
+    // ==============================
     @Test
     public void test_sort() {
         jedis = jedisPool.getResource();
     }
 
-    /**
-     * ==============================
-     * 队列和栈
-     * ==============================
-     */
+    // ==============================
+    //          队列和栈
+    // ==============================
     @Test
     public void test_stack() {
         jedis = jedisPool.getResource();
