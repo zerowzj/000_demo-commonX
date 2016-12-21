a0c5e132bf6657432cf9bd4d4e3a4fdfc9cac163
wangzhenjun <wangzhenjun@okay.cn>
2016-12-21 13:43:05 +0800

    更新代码

---
+++ common-util/src/test/java/com/study/jdk/jcf/ListTest.java
@@ -0,0 +1,28 @@
+package com.study.jdk.jcf;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by wangzhj on 2016/12/21.
+ */
+public class ListTest {
+
+    public static final Logger logger = LoggerFactory.getLogger(ListTest.class);
+
+    @Test
+    public void test_ArrayList(){
+
+    }
+
+    @Test
+    public void test_LinkedList(){
+
+    }
+
+    @Test
+    public void test_Vector(){
+
+    }
+}
---
+++ common-util/src/test/java/com/study/jdk/jcf/SetTest.java
@@ -0,0 +1,20 @@
+package com.study.jdk.jcf;
+
+import org.junit.Test;
+
+/**
+ * Created by wangzhj on 2016/12/21.
+ */
+public class SetTest {
+
+    /**
+     * HashSet使用的是相当复杂的方式来存储元素的，
+     * 使用HashSet能够最快的获取集合中的元素，效率非常高（以空间换时间）。
+     * 会根据hashcode和equals来庞端是否是同一个对象，
+     * 如果hashcode一样，并且equals返回true，则是同一个对象，不能重复存放。
+     */
+    @Test
+    public void test_HashSet() {
+
+    }
+}
