9b5f66f9d3de923ca09712461554fafeae43196f
wangzhenjun <wangzhenjun@okay.cn>
2016-12-21 11:38:50 +0800

    更新代码

---
+++ common-util/src/test/java/com/study/guava/primitives/FloatsTest.java
@@ -0,0 +1,17 @@
+package com.study.guava.primitives;
+
+import com.google.common.primitives.Floats;
+import com.google.common.primitives.Longs;
+import org.junit.Test;
+
+/**
+ * Created by wangzhj on 2016/12/20.
+ */
+public class FloatsTest {
+
+    @Test
+    public void test(){
+
+        Floats.asList(1,2,3);
+    }
+}
