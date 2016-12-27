c5fe649ddbf63dc117b0004eedebbe88dee4b72d
wangzhenjun <wangzhenjun@okay.cn>
2016-12-27 11:33:39 +0800

    更新代码

--- common-util/src/test/java/com/study/commons/codec/DigestTest.java
+++ common-util/src/test/java/com/study/commons/codec/DigestTest.java
@@ -15,7 +15,7 @@ public class DigestTest {
     @Test
     public void test_md5(){
 //        String str = "hs=1482677302218&InnerTime=240&PlayTime=2271&selectparam=16jxjy-dwl-zzjd-600_2016&VideoID=0103";
-        String str = "/cw/videoShow/auxiliaryModule/saveCwKcjyTime.html?selectparam=16jxjy-dwl-zzjd-600_2016&VideoID=0103&PlayTime=2271&InnerTime=240&hs=1482677302218";
+        String str = "selectparam=16jxjy-dwl-zzjd-600_2016&VideoID=0103&PlayTime=2271&InnerTime=240&hs=1482677302218";
         logger.info(DigestUtils.md5Hex(str));
         logger.info("8fc32e87935f87891e65a47d11bab847");
     }
