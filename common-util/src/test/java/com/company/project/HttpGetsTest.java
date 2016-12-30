3f6f6fe9fc9250b2b25000af0ccaba28a7f48912
wangzhenjun <wangzhenjun@okay.cn>
2016-12-30 10:38:53 +0800

    更新代码

--- common-util/src/test/java/com/company/project/HttpGetsTest.java
+++ common-util/src/test/java/com/company/project/HttpGetsTest.java
@@ -3,22 +3,56 @@ package com.company.project;
 import com.company.util.http.HttpGets;
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by wangzhj on 2016/12/29.
  */
 public class HttpGetsTest {
 
+    ExecutorService exec = Executors.newFixedThreadPool(100);
+
     @Test
     public void test_submit() {
-        for (int i = 0; i < 100; i++) {
-            HttpGets.create("http://www.baidu.com").submit();
+        long start = System.currentTimeMillis();
+        CountDownLatch latch = new CountDownLatch(20);
+        for (int i = 0; i < 100000; i++) {
+            latch.countDown();
+            exec.submit(new Runnable() {
+                @Override
+                public void run() {
+                    HttpGets.create("http://www.baidu.com").submit();
+                }
+            });
         }
+        try {
+            latch.await();
+        } catch (Exception ex) {
+
+        }
+        System.out.println("耗时：" + (System.currentTimeMillis() - start));
     }
 
     @Test
     public void test_asyncSubmit() {
-        for (int i = 0; i < 100; i++) {
-            HttpGets.create("http://www.baidu.com").asyncSubmit();
+        long start = System.currentTimeMillis();
+        CountDownLatch latch = new CountDownLatch(20);
+        for (int i = 0; i < 100000; i++) {
+            latch.countDown();
+            exec.submit(new Runnable() {
+                @Override
+                public void run() {
+                    HttpGets.create("http://www.baidu.com").asyncSubmit();
+                }
+            });
         }
+        try {
+            latch.await();
+        } catch (Exception ex) {
+
+        }
+        System.out.println("耗时：" + (System.currentTimeMillis() - start));
     }
 }
