ae405069802b594ba72f7ab000281903ae9dd26f
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2017-01-11 17:30:42 +0800

    更新代码

--- common-monitor/src/main/java/com/company/monitor/dubbo/ConsumerFilter.java
+++ common-monitor/src/main/java/com/company/monitor/dubbo/ConsumerFilter.java
@@ -2,7 +2,11 @@ package com.company.monitor.dubbo;
 
 import com.alibaba.dubbo.common.Constants;
 import com.alibaba.dubbo.common.extension.Activate;
-import com.alibaba.dubbo.rpc.*;
+import com.alibaba.dubbo.rpc.Filter;
+import com.alibaba.dubbo.rpc.Invocation;
+import com.alibaba.dubbo.rpc.Invoker;
+import com.alibaba.dubbo.rpc.Result;
+import com.alibaba.dubbo.rpc.RpcException;
 import com.company.util.JsonUtil;
 import com.google.common.base.Joiner;
 import com.google.common.base.Stopwatch;
