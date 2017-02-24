87472de69984892c301a6620da1727d6f53e803f
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2017-02-24 10:04:16 +0800

    更新代码

---
+++ common-client/main/java/com/company/client/zk/BaseDaoImpl.java
@@ -0,0 +1,41 @@
+package com.company.client.zk;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/**
+ * 基础Dao实现
+ *
+ * @author wangzhj
+ */
+abstract class BaseDaoImpl {
+
+    private String zkAddress;
+
+    private CuratorFramework client;
+
+    @PostConstruct
+    private void init() {
+        client = CuratorFrameworkFactory.newClient(zkAddress, new ExponentialBackoffRetry(1000, 3));
+        client.start();
+    }
+
+    @PreDestroy
+    private void destroy() {
+        if (client != null) {
+            client.close();
+        }
+    }
+
+    public CuratorFramework getClient() {
+        return client;
+    }
+
+    public void setZkAddress(String zkAddress) {
+        this.zkAddress = zkAddress;
+    }
+}
