4152c1888dc201e2d9561d352d503399e51fff73
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2017-03-06 09:45:52 +0800

    更新代码

---
+++ common-client/src/main/java/com/company/client/redis/RedisLists.java
@@ -0,0 +1,7 @@
+package com.company.client.redis;
+
+/**
+ * Created by wangzhj on 2017/3/6.
+ */
+public class RedisLists {
+}
---
+++ common-client/src/main/java/com/company/client/redis/RedisStrings.java
@@ -0,0 +1,11 @@
+package com.company.client.redis;
+
+/**
+ * Created by wangzhj on 2017/3/6.
+ */
+public class RedisStrings {
+
+    public void setString(){
+
+    }
+}
